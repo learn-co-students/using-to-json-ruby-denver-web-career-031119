a88e1946321bd7473695b175eb2d0f5db2c62559
Zach Pardee <zachpardee@gmail.com>
2019-05-03 11:11:09 -0600

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,10 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @post.to_json(only: [:title, :description, :id],
+                              include: [author: { only: [:name]}]) }
+    end
   end
 end
